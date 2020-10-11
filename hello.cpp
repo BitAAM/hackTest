329c94399f95550ae03b1e59e861f03d66cd6ecf
TechAugutsya <72683829+TechAugutsya@users.noreply.github.com>
2020-10-11 12:07:42 +0530

    Update hello.cpp
    Added multiplication function

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 
+int multiply(int a,int b){
+  return a*b;
+}
 int main(){
   cout<<"First C++ program";
+  cout<<"Multiplication of 5 and 6 is "<<multiply(5,6);
   return 0;
 }
