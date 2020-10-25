7bdfbe086b0ca190a62be9b5bc11c597b9bf4588
alaukikmishra <73432917+alaukikmishra@users.noreply.github.com>
2020-10-25 20:12:42 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ int multiply(int a,int b){
   return a*b;
 }
 int main(){
-  cout<<"First C++ program";
+  cout<<"First C++ program";//first
   cout<<"Multiplication of 5 and 6 is "<<multiply(5,6);//multiplication
   return 0;//return zero
 }
